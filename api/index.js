bcbc132934a09d6d20edb5ae23badb8bc63001de
Emmnia <eugmalikova@gmail.com>
2024-09-05 15:55:31 +0300

    111

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const dataFilePath = path.join(__dirname, "data.json");
+
+function loadData() {
+    if (fs.existsSync(dataFilePath)) {
+        const rawData = fs.readFileSync(dataFilePath);
+        return JSON.parse(rawData);
+    }
+    return [];
+}
+
+let characters = loadData();
+
+app.get("/", function (_, res) {
+    res.send(characters);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
