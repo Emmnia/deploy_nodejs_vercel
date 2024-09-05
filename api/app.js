240e03006721a1f67f50cc9c34823c43cc40c953
Emmnia <eugmalikova@gmail.com>
2024-09-05 17:47:13 +0300

    fix

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/api'));
 
 const dataFilePath = path.join(__dirname, "data.json");
 
